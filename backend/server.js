5762afdfb496a80cbc274a53c201ce5bbe999534
Manu Thakur <kmanu74841@gmail.com>
2026-04-03 12:08:11 +0530

    changes sucess

--- backend/server.js
+++ backend/server.js
@@ -32,5 +32,5 @@ app.delete('/api/users/:id', (req, res) => {
 });
 
 app.listen(5000, () => {
-    console.log("Server running on port 5000");
+    console.log("Server running on port 5000"); 
 });
